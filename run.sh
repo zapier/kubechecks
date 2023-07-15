b3e2f0835560856bee7496b18c80513b9125eeb0
Matt Morrison <matt.morrison@zapier.com>
2023-07-15 15:46:28 +1200

    Update run.sh
    Signed-off-by: Matt Morrison <matt@mojo.net.nz>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ARGOCD_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcmdvY2QiLCJzdWIiOiJhZG1pbjphcGlLZXkiLCJleHAiOjE2ODkzNzQyNjUsIm5iZiI6MTY4OTI4Nzg2NSwiaWF0IjoxNjg5Mjg3ODY1LCJqdGkiOiJrdWJlY2hlY2tzLXRlc3RpbmcifQ.RvdOi0J88aG0mUUxDZBnk9DY_UD_zXyiHBcMt7dB2dE"
-GITHUB_TOKEN="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"
+GITHUB_TOKEN="ghp_xyz......1hJ"
 
 go run ./main.go \
   --vcs-type github \
