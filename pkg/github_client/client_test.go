2791d43b67a6405123575999836ad0bab80fdd03
Mmadu Manasseh <manasseh.mmadu@zapier.com>
2023-07-18 12:06:42 +0100

    Update tests

---
+++ pkg/github_client/client_test.go
@@ -0,0 +1,16 @@
+package github_client
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestCleanupGetManifestsError tests the cleanupGetManifestsError function.
+func TestCreateClient(t *testing.T) {
+	viper.Set("vcs-token", "pass")
+	githubClient = createGithubClient()
+	assert.Equal(t, "https://api.github.com/", githubClient.BaseURL.String(), fmt.Sprintf("api URL in githubClient (%s) does not match github public API", githubClient.BaseURL.String()))
+}
