08b878e3d49cef6ecffab124d9aa109b6f816eca
Joseph Lombrozo <joe.lombrozo@zapier.com>
2023-10-10 20:43:47 -0400

    some refactoring on the list of actions (#71)

--- pkg/utils.go
+++ pkg/utils.go
@@ -1,34 +1,9 @@
 package pkg
 
-import (
-	"fmt"
-
-	argoappv1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
-	"github.com/argoproj/argo-cd/v2/reposerver/apiclient"
-	"github.com/ghodss/yaml"
-	"github.com/rs/zerolog/log"
-)
-
 var (
 	GitTag    = ""
 	GitCommit = ""
 )
-
-func BuildManifest(resp *apiclient.ManifestResponse) []string {
-	manifests := []string{}
-	for _, m := range resp.Manifests {
-		obj, err := argoappv1.UnmarshalToUnstructured(m)
-		if err != nil {
-			log.Warn().Msgf("error processing Argo manifest: %v", err)
-			continue
-		}
-
-		yamlBytes, _ := yaml.Marshal(obj)
-		manifests = append(manifests, fmt.Sprintf("---\n%s", string(yamlBytes)))
-	}
-
-	return manifests
-}
 
 func PassEmoji() string {
 	return " :white_check_mark: "
