39d83df88927c75b43729ae643321672a7978ae8
joe.lombrozo <joe.lombrozo@zapier.com>
2024-04-03 16:07:48 -0400

    add some tests

---
+++ pkg/affected_apps/multi_matcher_test.go
@@ -0,0 +1,102 @@
+package affected_apps
+
+import (
+	"context"
+	"testing"
+
+	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	"github.com/stretchr/testify/require"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type fakeMatcher struct {
+	items AffectedItems
+}
+
+func (f fakeMatcher) AffectedApps(ctx context.Context, changeList []string, targetBranch string) (AffectedItems, error) {
+	return f.items, nil
+}
+
+func TestMultiMatcher(t *testing.T) {
+	t.Run("exists in one but not two", func(t *testing.T) {
+		app1 := v1alpha1.Application{ObjectMeta: v1.ObjectMeta{Name: "app-1"}}
+		matcher1 := fakeMatcher{
+			items: AffectedItems{
+				Applications: []v1alpha1.Application{app1},
+			},
+		}
+		matcher2 := fakeMatcher{}
+
+		ctx := context.Background()
+		matcher := NewMultiMatcher(matcher1, matcher2)
+		total, err := matcher.AffectedApps(ctx, nil, "")
+
+		require.NoError(t, err)
+		require.Len(t, total.Applications, 1)
+		require.Equal(t, app1, total.Applications[0])
+	})
+
+	t.Run("exists in two but not one", func(t *testing.T) {
+		app1 := v1alpha1.Application{ObjectMeta: v1.ObjectMeta{Name: "app-1"}}
+		matcher1 := fakeMatcher{}
+		matcher2 := fakeMatcher{
+			items: AffectedItems{
+				Applications: []v1alpha1.Application{app1},
+			},
+		}
+
+		ctx := context.Background()
+		matcher := NewMultiMatcher(matcher1, matcher2)
+		total, err := matcher.AffectedApps(ctx, nil, "")
+
+		require.NoError(t, err)
+		require.Len(t, total.Applications, 1)
+		require.Equal(t, app1, total.Applications[0])
+	})
+
+	t.Run("exists in both", func(t *testing.T) {
+		app1 := v1alpha1.Application{ObjectMeta: v1.ObjectMeta{Name: "app-1"}}
+		matcher1 := fakeMatcher{
+			items: AffectedItems{
+				Applications: []v1alpha1.Application{app1},
+			},
+		}
+		matcher2 := fakeMatcher{
+			items: AffectedItems{
+				Applications: []v1alpha1.Application{app1},
+			},
+		}
+
+		ctx := context.Background()
+		matcher := NewMultiMatcher(matcher1, matcher2)
+		total, err := matcher.AffectedApps(ctx, nil, "")
+
+		require.NoError(t, err)
+		require.Len(t, total.Applications, 1)
+		require.Equal(t, app1, total.Applications[0])
+	})
+
+	t.Run("each contains unique app", func(t *testing.T) {
+		app1 := v1alpha1.Application{ObjectMeta: v1.ObjectMeta{Name: "app-1"}}
+		app2 := v1alpha1.Application{ObjectMeta: v1.ObjectMeta{Name: "app-2"}}
+		matcher1 := fakeMatcher{
+			items: AffectedItems{
+				Applications: []v1alpha1.Application{app1},
+			},
+		}
+		matcher2 := fakeMatcher{
+			items: AffectedItems{
+				Applications: []v1alpha1.Application{app2},
+			},
+		}
+
+		ctx := context.Background()
+		matcher := NewMultiMatcher(matcher1, matcher2)
+		total, err := matcher.AffectedApps(ctx, nil, "")
+
+		require.NoError(t, err)
+		require.Len(t, total.Applications, 2)
+		require.Equal(t, app1, total.Applications[0])
+		require.Equal(t, app2, total.Applications[1])
+	})
+}
