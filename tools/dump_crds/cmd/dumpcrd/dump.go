6686a36a9c1574019785b5ad208938bd48fc1ffb
James hong <james.hong@zapier.com>
2024-06-28 16:42:21 +1000

    fix ci and remove unwanted comments

--- tools/dump_crds/cmd/dumpcrd/dump.go
+++ tools/dump_crds/cmd/dumpcrd/dump.go
@@ -35,16 +35,6 @@ Used to generate openAPIV3Schema for each CRD in the cluster. To be consumed by 
 
 func init() {
 	rootCmd.AddCommand(dumpCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// dumpCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// dumpCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func dump() {
