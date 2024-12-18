53a514f07faaee713468b3e08e668a32df3fd335
Mmadu Manasseh <manasseh.mmadu@zapier.com>
2024-12-18 04:13:07 +0100

    Initial support for kyverno policies
    Signed-off-by: Mmadu Manasseh <manasseh.mmadu@zapier.com>

--- cmd/controller.go
+++ cmd/controller.go
@@ -79,6 +79,12 @@ var ControllerCmd = &cobra.Command{
 			log.Fatal().Err(err).Msg("failed to process schema locations")
 		}
 
+		log.Info().Strs("locations", cfg.KyvernoPoliciesLocation).Msg("processing kyverno policies locations")
+		if err = processLocations(ctx, ctr, cfg.KyvernoPoliciesLocation); err != nil {
+			log.Fatal().Err(err).Msg("failed to process kyverno policies locations")
+		}
+		log.Debug().Strs("locations", cfg.KyvernoPoliciesLocation).Msg("kyverno policies locations after processing")
+
 		processors, err := getProcessors(ctr)
 		if err != nil {
 			log.Fatal().Err(err).Msg("failed to create processors")
