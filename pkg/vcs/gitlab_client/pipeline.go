7d8de35473bb3c11872e749bcbe97b08f56950a5
Joe Lombrozo <joe.lombrozo@zapier.com>
2025-01-09 11:35:34 -0500

    add a bunch more debugging

--- pkg/vcs/gitlab_client/pipeline.go
+++ pkg/vcs/gitlab_client/pipeline.go
@@ -16,32 +16,39 @@ func (c *Client) GetLastPipelinesForCommit(projectName string, commitSHA string)
 		return nil
 	}
 
-	if len(pipelines) > 0 {
-		// check for merge_requests_event
-		for _, p := range pipelines {
-			if p.Source == "merge_request_event" {
-				return p
-			}
-		}
+	log.Debug().Int("pipline_count", len(pipelines)).Msg("gitlab client: retrieve pipelines for commit")
 
-		// check for external_pull_request_events next
-		for _, p := range pipelines {
-			if p.Source == "pipeline" {
-				return p
-			}
-		}
+	for _, p := range pipelines {
+		log.Debug().
+			Int("pipeline_id", p.ID).
+			Str("source", p.Source).
+			Msg("gitlab client: pipeline details")
+	}
 
-		// check for external_pull_request_events next
-		for _, p := range pipelines {
-			if p.Source == "external_pull_request_event" {
-				return p
-			}
+	// check for merge_requests_event
+	for _, p := range pipelines {
+		if p.Source == "merge_request_event" {
+			return p
 		}
+	}
 
-		for _, p := range pipelines {
-			if p.Source == "external" {
-				return p
-			}
+	// check for external_pull_request_events next
+	for _, p := range pipelines {
+		if p.Source == "pipeline" {
+			return p
+		}
+	}
+
+	// check for external_pull_request_events next
+	for _, p := range pipelines {
+		if p.Source == "external_pull_request_event" {
+			return p
+		}
+	}
+
+	for _, p := range pipelines {
+		if p.Source == "external" {
+			return p
 		}
 	}
 
