1b2c10df827c9f42c1a84abbd1b33891defad48a
James Hong <james.hong@zapier.com>
2025-02-01 00:33:15 +1000

    simplify kustomize walk (#361)
    1. instead of performing addFile, addDir inside kustomize package, it will return lists of files and dir thats used in kustomization.yaml
    2. add and updated unit tests
    3. several MR tests are now using outdated ingress api version, this is now updated)
    4. tool-version is now updated

--- pkg/argo_client/kustomize.go
+++ pkg/argo_client/kustomize.go
@@ -6,32 +6,21 @@ import (
 	"path/filepath"
 
 	"github.com/pkg/errors"
-	"github.com/zapier/kubechecks/pkg/kustomize"
-	"sigs.k8s.io/kustomize/kyaml/filesys"
 )
 
-type processor struct {
-	repoRoot string
-	tempDir  string
-	repoFS   filesys.FileSystem
-}
-
-func (p processor) AddDir(s string) error {
-	return nil
-}
-
-func (p processor) AddFile(relPath string) error {
-	absDepPath := filepath.Clean(filepath.Join(p.repoRoot, relPath))
+// addFile copies a file from the repository to the temp directory to prepare to be sent to the ArgoCD API.
+func addFile(repoRoot string, tempDir string, relPath string) error {
+	absDepPath := filepath.Clean(filepath.Join(repoRoot, relPath))
 
 	// Get relative path from repo root
-	relPath, err := filepath.Rel(p.repoRoot, absDepPath)
+	relPath, err := filepath.Rel(repoRoot, absDepPath)
 	if err != nil {
 		return errors.Wrapf(err, "failed to get relative path for %s", absDepPath)
 	}
 
 	// check if the file exists in the temp directory
 	// skip copying if it exists
-	tempPath := filepath.Join(p.tempDir, relPath)
+	tempPath := filepath.Join(tempDir, relPath)
 	if _, err := os.Stat(tempPath); err == nil {
 		return nil
 	}
@@ -63,5 +52,3 @@ func (p processor) AddFile(relPath string) error {
 	_, err = io.Copy(w, r)
 	return errors.Wrap(err, "failed to copy file")
 }
-
-var _ kustomize.Processor = new(processor)
