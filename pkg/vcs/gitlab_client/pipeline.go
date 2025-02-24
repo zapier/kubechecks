8a8ebbe59f10f4d42712d51e4313146f27e7e73c
Joseph Lombrozo <joe.lombrozo@zapier.com>
2025-02-24 11:34:27 -0500

    log pipeline errors when they occur (#341)

--- pkg/vcs/gitlab_client/pipeline.go
+++ pkg/vcs/gitlab_client/pipeline.go
@@ -7,51 +7,48 @@ import (
 	"github.com/zapier/kubechecks/pkg"
 )
 
-func (c *Client) GetPipelinesForCommit(projectName string, commitSHA string) ([]*gitlab.PipelineInfo, error) {
+func (c *Client) GetLastPipelinesForCommit(projectName string, commitSHA string) *gitlab.PipelineInfo {
 	pipelines, _, err := c.c.Pipelines.ListProjectPipelines(projectName, &gitlab.ListProjectPipelinesOptions{
 		SHA: pkg.Pointer(commitSHA),
 	})
 	if err != nil {
-		log.Error().Err(err).Msg("gitlab client: could not get pipelines for commit")
-		return pipelines, err
-	}
-
-	return pipelines, nil
-
-}
-
-func (c *Client) GetLastPipelinesForCommit(projectName string, commitSHA string) *gitlab.PipelineInfo {
-	pipelines, err := c.GetPipelinesForCommit(projectName, commitSHA)
-	if err != nil {
+		log.Error().Err(err).Msg("gitlab client: could not get last pipeline for commit")
 		return nil
 	}
 
-	if len(pipelines) > 0 {
-		// check for merge_requests_event
-		for _, p := range pipelines {
-			if p.Source == "merge_request_event" {
-				return p
-			}
-		}
+	log.Debug().Int("pipline_count", len(pipelines)).Msg("gitlab client: retrieve pipelines for commit")
 
-		// check for external_pull_request_events next
-		for _, p := range pipelines {
-			if p.Source == "pipeline" {
-				return p
-			}
-		}
+	for _, p := range pipelines {
+		log.Debug().
+			Int("pipeline_id", p.ID).
+			Str("source", p.Source).
+			Msg("gitlab client: pipeline details")
+	}
 
-		// check for external_pull_request_events next
-		for _, p := range pipelines {
-			if p.Source == "external_pull_request_event" {
-				return p
-			}
+	// check for merge_requests_event
+	for _, p := range pipelines {
+		if p.Source == "merge_request_event" {
+			return p
 		}
+	}
 
-		for _, p := range pipelines {
-			if p.Source == "external" {
-				return p
-			}
+	// check for external_pull_request_events next
+	for _, p := range pipelines {
+		if p.Source == "pipeline" {
+			return p
+		}
+	}
+
+	// check for external_pull_request_events next
+	for _, p := range pipelines {
+		if p.Source == "external_pull_request_event" {
+			return p
+		}
+	}
+
+	for _, p := range pipelines {
+		if p.Source == "external" {
+			return p
 		}
 	}
 
