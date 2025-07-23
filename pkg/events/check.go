9979e7897a3708d6398b55a09bf0edf0a85c07ef
jeverakes_Aledade <jeverakes@aledade.com>
2025-07-23 07:32:35 -0700

    removed posting to repo PR's with no app
    Signed-off-by: jeverakes_Aledade <jeverakes@aledade.com>

--- pkg/events/check.go
+++ pkg/events/check.go
@@ -275,10 +275,6 @@ func (ce *CheckEvent) Process(ctx context.Context) error {
 
 	if len(ce.affectedItems.Applications) <= 0 && len(ce.affectedItems.ApplicationSets) <= 0 {
 		ce.logger.Info().Msg("No affected apps or appsets, skipping")
-		if _, err := ce.ctr.VcsClient.PostMessage(ctx, ce.pullRequest, fmt.Sprintf("## Kubechecks %s Report\nNo changes", ce.ctr.Config.Identifier)); err != nil {
-			return errors.Wrap(err, "failed to post changes")
-		}
-		return nil
 	}
 
 	// We make one comment per run, containing output for all the apps
