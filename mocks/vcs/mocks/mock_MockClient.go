df5ff2108fbcc46d0152d04cd32603d72b9e13d2
Joe Lombrozo <joe@djeebus.net>
2026-08-19 19:41:08 -0700

    fix the clone so that it also uses github app auth, when appropriate (#543)
    * fix the clone so that it also uses github app auth, when appropriate
    
    * add tests, clean up wrapped error message
    
    * remove dupe

--- mocks/vcs/mocks/mock_MockClient.go
+++ mocks/vcs/mocks/mock_MockClient.go
@@ -389,6 +389,72 @@ func (_c *MockClient_GetAuthHeaders_Call) RunAndReturn(run func(ctx context.Cont
 	return _c
 }
 
+// GitCredentials provides a mock function for the type MockClient
+func (_mock *MockClient) GitCredentials(ctx context.Context) (string, string, error) {
+	ret := _mock.Called(ctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for GitCredentials")
+	}
+
+	var r0 string
+	var r1 string
+	var r2 error
+	if returnFunc, ok := ret.Get(0).(func(context.Context) (string, string, error)); ok {
+		return returnFunc(ctx)
+	}
+	if returnFunc, ok := ret.Get(0).(func(context.Context) string); ok {
+		r0 = returnFunc(ctx)
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+	if returnFunc, ok := ret.Get(1).(func(context.Context) string); ok {
+		r1 = returnFunc(ctx)
+	} else {
+		r1 = ret.Get(1).(string)
+	}
+	if returnFunc, ok := ret.Get(2).(func(context.Context) error); ok {
+		r2 = returnFunc(ctx)
+	} else {
+		r2 = ret.Error(2)
+	}
+	return r0, r1, r2
+}
+
+// MockClient_GitCredentials_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'GitCredentials'
+type MockClient_GitCredentials_Call struct {
+	*mock.Call
+}
+
+// GitCredentials is a helper method to define mock.On call
+//   - ctx context.Context
+func (_e *MockClient_Expecter) GitCredentials(ctx interface{}) *MockClient_GitCredentials_Call {
+	return &MockClient_GitCredentials_Call{Call: _e.mock.On("GitCredentials", ctx)}
+}
+
+func (_c *MockClient_GitCredentials_Call) Run(run func(ctx context.Context)) *MockClient_GitCredentials_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		run(
+			arg0,
+		)
+	})
+	return _c
+}
+
+func (_c *MockClient_GitCredentials_Call) Return(s string, s1 string, err error) *MockClient_GitCredentials_Call {
+	_c.Call.Return(s, s1, err)
+	return _c
+}
+
+func (_c *MockClient_GitCredentials_Call) RunAndReturn(run func(ctx context.Context) (string, string, error)) *MockClient_GitCredentials_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // GetHookByUrl provides a mock function for the type MockClient
 func (_mock *MockClient) GetHookByUrl(ctx context.Context, repoName string, webhookUrl string) (*vcs.WebHookConfig, error) {
 	ret := _mock.Called(ctx, repoName, webhookUrl)
